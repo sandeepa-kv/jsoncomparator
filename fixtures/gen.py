a290415a7462b7b307f5900511cc46f2a25a727c
svasudevajois <svasudevajois@sandeepa-inl.corp.bluejeans.com>
2020-02-14 11:51:33 +0530

    Updating link generator to use reqres.in REST APIs

--- fixtures/gen.py
+++ fixtures/gen.py
@@ -2,14 +2,16 @@ import random
 
 file_1 = open('file_1', 'w')
 file_2 = open('file_2', 'w')
-number_of_lines = 100
+number_of_lines = 1000
 
 count_equal = 0
 count_not_equal = 0
 
 for i in range(0, number_of_lines):
-    url_1 = ("https://jsonplaceholder.typicode.com/photos/%s\n" % (random.randint(1, 5000)))
-    url_2 = ("https://jsonplaceholder.typicode.com/photos/%s\n" % (random.randint(1, 5000)))
+    # url_1 = ("https://jsonplaceholder.typicode.com/photos/%s\n" % (random.randint(1, 5000)))
+    # url_2 = ("https://jsonplaceholder.typicode.com/photos/%s\n" % (random.randint(1, 5000)))
+    url_1 = ("https://reqres.in/api/users/%s\n" % (random.randint(1, 12)))
+    url_2 = ("https://reqres.in/api/users/%s\n" % (random.randint(1, 12)))
 
     if random.choice([True, False]):
         file_1.write(url_1)
